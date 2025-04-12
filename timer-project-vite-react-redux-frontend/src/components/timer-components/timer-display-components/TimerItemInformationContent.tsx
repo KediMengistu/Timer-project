c63eb97bfbfcd082a96f56f152b9b1ace7579ce6
KediMengistu <kedimengistu@gmail.com>
2025-04-12 07:34:30 -0400

    Refactor Timer Information Break List Content Display - 10.0

--- timer-project-vite-react-redux-frontend/src/components/timer-components/timer-display-components/TimerItemInformationContent.tsx
+++ timer-project-vite-react-redux-frontend/src/components/timer-components/timer-display-components/TimerItemInformationContent.tsx
@@ -107,7 +107,7 @@ function TimerItemInformationContent({
             </div>
             <div
               // ref={utcScrollContainerRef}
-              className="relative flex h-full w-full snap-x snap-mandatory scroll-p-1! items-center overflow-x-auto border-t-2 border-black pt-1! pr-1! pb-2! pl-1! whitespace-nowrap dark:border-gray-700 dark:bg-transparent [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent dark:[&::-webkit-scrollbar-track]:bg-transparent"
+              className="relative m-1! flex h-full w-full snap-x snap-mandatory scroll-p-1! items-center overflow-x-auto border-t-2 border-black pt-1! pr-1! pb-2! pl-1! whitespace-nowrap dark:border-gray-700 dark:bg-transparent [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent dark:[&::-webkit-scrollbar-track]:bg-transparent"
             >
               <div className="ml-2! inline h-full w-full shrink-0 grow-0 snap-center snap-always rounded-sm border-1 border-black bg-white align-middle whitespace-normal shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-gray-700 dark:bg-gray-800">
                 <div className="grid h-full w-full grid-rows-10 gap-1 p-1!">
@@ -259,7 +259,7 @@ function TimerItemInformationContent({
             </div>
             <div
               // ref={localScrollContainerRef}
-              className="relative flex h-full w-full snap-x snap-mandatory scroll-p-1! items-center overflow-x-auto border-t-2 border-black pt-1! pr-1! pb-2! pl-1! whitespace-nowrap dark:border-gray-700 dark:bg-transparent [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent dark:[&::-webkit-scrollbar-track]:bg-transparent"
+              className="relative m-1! flex h-full w-full snap-x snap-mandatory scroll-p-1! items-center overflow-x-auto border-t-2 border-black pt-1! pr-1! pb-2! pl-1! whitespace-nowrap dark:border-gray-700 dark:bg-transparent [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400 dark:[&::-webkit-scrollbar-thumb]:bg-gray-400 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent dark:[&::-webkit-scrollbar-track]:bg-transparent"
             >
               <div className="ml-2! inline h-full w-full shrink-0 grow-0 snap-center snap-always rounded-sm border-1 border-black bg-white align-middle whitespace-normal shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-gray-700 dark:bg-gray-800">
                 <div className="grid h-full w-full grid-rows-10 gap-1 p-1!">
